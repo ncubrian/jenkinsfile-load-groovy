4bed002b6ac6e038b9b7f44b266f185117d824e8
radu-matei <matei.radu94@gmail.com>
2017-06-19 17:23:55 +0300

    Add groovy method to add jnlp slave
    Signed-off by: Radu Matei<matei.radu94@gmail.com
    
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

---
+++ addNode.groovy
@@ -0,0 +1,20 @@
+import hudson.model.Node.Mode
+import hudson.slaves.*
+import jenkins.model.Jenkins
+
+
+def addLinuxNode(String agentName) {
+
+    DumbSlave dumb = new DumbSlave(agentName,  // Agent name, usually matches the host computer's machine name
+            "",           // Agent description
+            "/home/jenkins",                  // Workspace on the agent's computer
+            agentExecutors,             // Number of executors
+            Mode.EXCLUSIVE,             // "Usage" field, EXCLUSIVE is "only tied to node", NORMAL is "any"
+            "linux-docker",             // Labels
+            new JNLPLauncher(),         // Launch strategy, JNLP is the Java Web Start setting services use
+            RetentionStrategy.INSTANCE) // Is the "Availability" field and INSTANCE means "Always"
+
+    Jenkins.instance.addNode(dumb)
+}
+
+return this;
