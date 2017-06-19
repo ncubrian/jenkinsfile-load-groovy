f01203d4297926182c02c4cb869d09a704f6a193
radu-matei <matei.radu94@gmail.com>
2017-06-19 17:39:51 +0300

    Update agent constructor
    Signed-off by: Radu Matei<matei.radu94@gmail.com
    
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

--- addNode.groovy
+++ addNode.groovy
@@ -5,15 +5,7 @@ import jenkins.model.Jenkins
 
 def addLinuxNode(String agentName) {
 
-    DumbSlave dumb = new DumbSlave(agentName,  // Agent name, usually matches the host computer's machine name
-            "",           // Agent description
-            "/home/jenkins",                  // Workspace on the agent's computer
-            2,             // Number of executors
-            Mode.EXCLUSIVE,             // "Usage" field, EXCLUSIVE is "only tied to node", NORMAL is "any"
-            "linux-docker",             // Labels
-            new JNLPLauncher(),         // Launch strategy, JNLP is the Java Web Start setting services use
-            RetentionStrategy.INSTANCE) // Is the "Availability" field and INSTANCE means "Always"
-
+    DumbSlave dumb = new DumbSlave(agentName, "/home/jenkins", new JNLPLauncher())
     Jenkins.instance.addNode(dumb)
 }
 
