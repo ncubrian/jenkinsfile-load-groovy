392e3a4f15b0f5b7a635e3033d746ad3b0a58ec7
radu-matei <matei.radu94@gmail.com>
2017-06-19 17:33:33 +0300

    Update executor count
    Signed-off by: Radu Matei<matei.radu94@gmail.com
    
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

--- addNode.groovy
+++ addNode.groovy
@@ -8,7 +8,7 @@ def addLinuxNode(String agentName) {
     DumbSlave dumb = new DumbSlave(agentName,  // Agent name, usually matches the host computer's machine name
             "",           // Agent description
             "/home/jenkins",                  // Workspace on the agent's computer
-            agentExecutors,             // Number of executors
+            2,             // Number of executors
             Mode.EXCLUSIVE,             // "Usage" field, EXCLUSIVE is "only tied to node", NORMAL is "any"
             "linux-docker",             // Labels
             new JNLPLauncher(),         // Launch strategy, JNLP is the Java Web Start setting services use
