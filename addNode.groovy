78e00ef86ddabb42220a44c282e1176e0a741565
radu-matei <matei.radu94@gmail.com>
2017-06-20 15:22:40 +0300

    Set number of executors for slave
    Signed-off by: Radu Matei<matei.radu94@gmail.com
    
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

--- addNode.groovy
+++ addNode.groovy
@@ -3,9 +3,10 @@ import hudson.slaves.*
 import jenkins.model.Jenkins
 
 
-public String addLinuxNode(String agentName) {
+public String addLinuxNode(String agentName, int numOfExecutors) {
 
     DumbSlave dumb = new DumbSlave(agentName, "/home/jenkins", new JNLPLauncher())
+    dumb.setNumExecutors(numOfExecutors)
     Jenkins.instance.addNode(dumb)
 
     return dumb.getComputer().getJnlpMac()
